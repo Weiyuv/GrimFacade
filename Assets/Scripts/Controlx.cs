81035bffb2ba48d8a832972c1c70ea4b525881a8
Bruno Chen <998isyi@gmail.com>
2024-10-29 08:06:41 -0300

    a

--- Assets/Scripts/Controlx.cs
+++ Assets/Scripts/Controlx.cs
@@ -2,7 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Controlx : MonoBehaviour
+public class Controlx
+    : MonoBehaviour
 {
     public Animator anima; // Referência ao Animator do personagem.
     float xmov; // Variável para guardar o movimento horizontal.
